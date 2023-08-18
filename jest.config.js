051f034a442d38af047c9ae6d92f03636f1fa6c8
Carlos_Vinicius2340 <cvs.silva@discente.ufma.br>
2023-08-18 14:40:49 -0300

    refactor: change imports to use module

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
   testEnvironment: 'node',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
+  transform: {
+    '.+\\.ts?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+  },
 };
