8cdec6e65a8337b7c70897fed40f0ba14baf2cca
Carlos_Vinicius2340 <CarlosVinicius3258@outlook.com>
2023-08-24 00:20:04 -0300

    chore: config sass

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+module.exports = 
+{
+  mode: "devolopment",
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [{
+      test: /\ts(x??)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    },
+      {
+        test: /\.scss$/
+        ,
+        use: [
+          {
+            loader: 'style-loader',
+            options: {
+              modules: true
+            }
+}]      }
+    ],
+    
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
