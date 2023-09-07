06857b4ad599891600ecca593016844c952503d4
Carlos_Vinicius2340 <CarlosVinicius3258@outlook.com>
2023-09-07 00:11:49 -0300

    test: ensure login start with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -7,4 +7,14 @@ describe('Login Component', () => {
     const errorWrap = getByTestId('error-wrap');
     expect(errorWrap.childElementCount).toBe(0);
   });
+  test('Should start with initial state', () => {
+    const { getByTestId } = render(<Login />);
+    const errorWrap = getByTestId('error-wrap');
+    expect(errorWrap.childElementCount).toBe(0);
+
+    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    expect(submitButton.disabled).toBe(true);
+  });
+
+
 });
