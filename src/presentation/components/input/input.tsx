10b68154db72df0c8cf1fe4e0c23a65e696cda16
Carlos_Vinicius2340 <CarlosVinicius3258@outlook.com>
2023-09-07 00:51:24 -0300

    feat: ensure login start with submit button disabled

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,19 +1,30 @@
-import React, { memo } from 'react';
+import React, { useContext } from 'react';
 import Styles from './input-styles.module.scss';
+import Context from '@/presentation/context/form/form-context';
 interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
-
 }
 
 const Input: React.FC<InputProps> = (props) => {
-
+  const { errorState } = useContext(Context);
+  const value = errorState;
+  const error = value[`${props.name}`];
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false;
+  };
+
+  const getStatus = (): string => {
+    return '🔴';
+  };
+
+  const getTitle = (): string => {
+    console.log('title', error, value);
+    return error;
   };
 
   return (
     <div className={ Styles.inputWrap }>
       <input readOnly onFocus={ enableInput } { ...props } />
-      <span className={ Styles.status }>🔴</span>
+      <span data-testid={ `${props.name}-status` } title={ getTitle() } className={ Styles.status }>{ getStatus() }</span>
     </div>
   );
 };
