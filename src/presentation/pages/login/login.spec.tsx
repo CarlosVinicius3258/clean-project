cc8fad72c37c5491e17d3384281d8e11941763e5
Carlos_Vinicius2340 <CarlosVinicius3258@outlook.com>
2023-09-06 23:21:28 -0300

    chore: add testing librares

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import Login from './login';
+describe('Login Component', () => {
+  test('', () => {
+    render(<Login />);
+  });
+});
